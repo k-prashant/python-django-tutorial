2bc3adbf78f5071f0b485ef58b55f70178786e3b
prashant <prashant@antfarm.in>
2017-06-21 14:27:55 +0530

    Sixth commit

--- first_proj/first_proj/settings.py
+++ first_proj/first_proj/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
