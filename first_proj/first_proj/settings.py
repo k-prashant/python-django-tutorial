abe0d9292f95547816d6e1bd269024b566c4db7f
prashant <prashant@antfarm.in>
2017-06-21 14:33:29 +0530

    Seventh commit

--- first_proj/first_proj/settings.py
+++ first_proj/first_proj/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    # os.path.join(PROJECT_DIR, 'static'),
 )
 
 
